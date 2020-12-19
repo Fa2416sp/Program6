b0ed4f1be1afa8327f068cea53afa396439b2522
Alex <56237625+Yang2114@users.noreply.github.com>
2020-12-19 04:57:28 +0300

    Добавьте файлы проекта.

---
+++ Program6/Form1.Designer.cs
@@ -0,0 +1,393 @@
+﻿
+namespace Program6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.A1 = new System.Windows.Forms.PictureBox();
+            this.A2 = new System.Windows.Forms.PictureBox();
+            this.A3 = new System.Windows.Forms.PictureBox();
+            this.A12 = new System.Windows.Forms.PictureBox();
+            this.A13 = new System.Windows.Forms.PictureBox();
+            this.A23 = new System.Windows.Forms.PictureBox();
+            this.A123 = new System.Windows.Forms.PictureBox();
+            this.zagatovka = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.A1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A13)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A23)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A123)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zagatovka)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(516, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Исходные данные: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(528, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Кординаты A1:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(519, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(518, 102);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(518, 187);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(519, 161);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(528, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Кординаты A2:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(518, 273);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(519, 247);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(528, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Кординаты A3:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(484, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "X1 =";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(483, 105);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Y1 =";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(483, 190);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Y2 =";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(484, 164);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(29, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "X2 =";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(483, 276);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(29, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Y3 =";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(484, 250);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(29, 13);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "X3 =";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(473, 311);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Выполнить сложные вычисления";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(12, 326);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(455, 20);
+            this.textBox7.TabIndex = 18;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(22, 310);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(178, 13);
+            this.label11.TabIndex = 19;
+            this.label11.Text = "Вывод по сложным вычислениям";
+            // 
+            // A1
+            // 
+            this.A1.Image = ((System.Drawing.Image)(resources.GetObject("A1.Image")));
+            this.A1.Location = new System.Drawing.Point(12, 12);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(442, 265);
+            this.A1.TabIndex = 20;
+            this.A1.TabStop = false;
+            // 
+            // A2
+            // 
+            this.A2.Image = ((System.Drawing.Image)(resources.GetObject("A2.Image")));
+            this.A2.Location = new System.Drawing.Point(13, 13);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(441, 264);
+            this.A2.TabIndex = 21;
+            this.A2.TabStop = false;
+            // 
+            // A3
+            // 
+            this.A3.Image = ((System.Drawing.Image)(resources.GetObject("A3.Image")));
+            this.A3.Location = new System.Drawing.Point(13, 13);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(441, 264);
+            this.A3.TabIndex = 22;
+            this.A3.TabStop = false;
+            // 
+            // A12
+            // 
+            this.A12.Image = ((System.Drawing.Image)(resources.GetObject("A12.Image")));
+            this.A12.Location = new System.Drawing.Point(12, 12);
+            this.A12.Name = "A12";
+            this.A12.Size = new System.Drawing.Size(442, 265);
+            this.A12.TabIndex = 23;
+            this.A12.TabStop = false;
+            // 
+            // A13
+            // 
+            this.A13.Image = ((System.Drawing.Image)(resources.GetObject("A13.Image")));
+            this.A13.Location = new System.Drawing.Point(12, 13);
+            this.A13.Name = "A13";
+            this.A13.Size = new System.Drawing.Size(442, 264);
+            this.A13.TabIndex = 24;
+            this.A13.TabStop = false;
+            // 
+            // A23
+            // 
+            this.A23.Image = ((System.Drawing.Image)(resources.GetObject("A23.Image")));
+            this.A23.Location = new System.Drawing.Point(13, 12);
+            this.A23.Name = "A23";
+            this.A23.Size = new System.Drawing.Size(441, 265);
+            this.A23.TabIndex = 25;
+            this.A23.TabStop = false;
+            // 
+            // A123
+            // 
+            this.A123.Image = ((System.Drawing.Image)(resources.GetObject("A123.Image")));
+            this.A123.Location = new System.Drawing.Point(12, 12);
+            this.A123.Name = "A123";
+            this.A123.Size = new System.Drawing.Size(442, 265);
+            this.A123.TabIndex = 26;
+            this.A123.TabStop = false;
+            // 
+            // zagatovka
+            // 
+            this.zagatovka.Image = ((System.Drawing.Image)(resources.GetObject("zagatovka.Image")));
+            this.zagatovka.Location = new System.Drawing.Point(13, 12);
+            this.zagatovka.Name = "zagatovka";
+            this.zagatovka.Size = new System.Drawing.Size(442, 265);
+            this.zagatovka.TabIndex = 27;
+            this.zagatovka.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(531, 340);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 28;
+            this.button2.Text = "Выход";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(670, 374);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.zagatovka);
+            this.Controls.Add(this.A123);
+            this.Controls.Add(this.A23);
+            this.Controls.Add(this.A13);
+            this.Controls.Add(this.A12);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.A1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.A1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A13)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A23)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A123)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zagatovka)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.PictureBox A1;
+        private System.Windows.Forms.PictureBox A2;
+        private System.Windows.Forms.PictureBox A3;
+        private System.Windows.Forms.PictureBox A12;
+        private System.Windows.Forms.PictureBox A13;
+        private System.Windows.Forms.PictureBox A23;
+        private System.Windows.Forms.PictureBox A123;
+        private System.Windows.Forms.PictureBox zagatovka;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Program6/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            zagatovka.Visible = true;
+            A1.Visible = false;
+            A2.Visible = false;
+            A3.Visible = false;
+            A12.Visible = false;
+            A13.Visible = false;
+            A23.Visible = false;
+            A123.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a1, a2, a3, x1, x2, x3, y1, y2, y3, d;
+            //A1
+            x1 = Convert.ToDouble(textBox1.Text);
+            y1 = Convert.ToDouble(textBox2.Text);
+            //A2
+            x2 = Convert.ToDouble(textBox4.Text);
+            y2 = Convert.ToDouble(textBox3.Text);
+            //A3
+            x3 = Convert.ToDouble(textBox6.Text);
+            y3 = Convert.ToDouble(textBox5.Text);
+            //Вычесления 
+            a1 = Math.Sqrt(Math.Pow(x1, 2) + Math.Pow(y1, 2));
+            a2 = Math.Sqrt(Math.Pow(x2, 2) + Math.Pow(y2, 2));
+            a3 = Math.Sqrt(Math.Pow(x3, 2) + Math.Pow(y3, 2));
+            //Вывод значений 
+            if (a1 > a2 & a1 > a3)
+            {
+                textBox7.Text = "точка A1 дальше от начала координат";
+                zagatovka.Visible = false;
+
+                zagatovka.Visible = false;
+                A1.Visible = true;
+                A2.Visible = false;
+                A3.Visible = false;
+                A12.Visible = false;
+                A13.Visible = false;
+                A23.Visible = false;
+                A123.Visible = false;
+            }
+            if (a2 > a1 & a2 > a3)
+            {
+                textBox7.Text = "точка A2 дальше от начала координат";
+                zagatovka.Visible = false;
+                A1.Visible = false;
+                A2.Visible = true;
+                A3.Visible = false;
+                A12.Visible = false;
+                A13.Visible = false;
+                A23.Visible = false;
+                A123.Visible = false;
+            }
+            if (a3 > a1 & a3 > a2)
+            {
+                textBox7.Text = "точка A3 дальше от начала координат";
+                zagatovka.Visible = false;
+                A1.Visible = false;
+                A2.Visible = false;
+                A3.Visible = true;
+                A12.Visible = false;
+                A13.Visible = false;
+                A23.Visible = false;
+                A123.Visible = false;
+            }
+            if (a1 == a2)
+            {
+                textBox7.Text = "точки A1 и A2 равноудалены от начала координат";
+                zagatovka.Visible = false;
+                A1.Visible = false;
+                A2.Visible = false;
+                A3.Visible = false;
+                A12.Visible = true;
+                A13.Visible = false;
+                A23.Visible = false;
+                A123.Visible = false;
+            }
+            if (a1 == a3)
+            {
+                textBox7.Text = "точки A1 и A3 равноудалены от начала координат";
+                zagatovka.Visible = false;
+                A1.Visible = false;
+                A2.Visible = false;
+                A3.Visible = false;
+                A12.Visible = false;
+                A13.Visible = true;
+                A23.Visible = false;
+                A123.Visible = false;
+            }
+            if (a2 == a3)
+            {
+                textBox7.Text = "точки A2 и A3 равноудалены от начала координат";
+                zagatovka.Visible = false;
+                A1.Visible = false;
+                A2.Visible = false;
+                A3.Visible = false;
+                A12.Visible = false;
+                A13.Visible = false;
+                A23.Visible = true;
+                A123.Visible = false;
+            }
+            if (a1 == a2 & a1 == a3 & a2 == a3)
+            {
+                textBox7.Text = "точки равноудалены от начала координат";
+                zagatovka.Visible = false;
+                A1.Visible = false;
+                A2.Visible = false;
+                A3.Visible = false;
+                A12.Visible = false;
+                A13.Visible = false;
+                A23.Visible = false;
+                A123.Visible = true;
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+       
+    }
+}
